a225c89dcaac20c7c31b3bec598eb9cdde7ed8aa
Vicente Agüero <vicenteaguero@uc.cl>
2025-02-05 22:40:28 -0300

    feat(website): :sparkles: Added placeholder for Methodology Page for React App

---
+++ website/src/pages/Methodology.jsx
@@ -0,0 +1,12 @@
+// website/src/pages/Methodology.jsx
+
+const Methodology = () => {
+  return (
+    <div className="methodology">
+      <h2>Metodología</h2>
+      <p>Información sobre la metodología de SmartPAES.</p>
+    </div>
+  );
+};
+
+export default Methodology;
