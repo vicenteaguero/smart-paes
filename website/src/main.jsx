685a2170a4e3e1341e0a9c51ea1d774822546c99
Vicente Agüero <vicenteaguero@uc.cl>
2025-02-05 22:37:35 -0300

    feat(website): :sparkles: Added Main JSX for React App

---
+++ website/src/main.jsx
@@ -0,0 +1,17 @@
+// website/src/main.jsx
+
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import { BrowserRouter } from "react-router-dom";
+import "./styles/base.css";
+import "./styles/nav.css";
+import "./styles/footer.css";
+import "./styles/home.css";
+import "./styles/ensayos.css";
+import "./styles/methodology.css";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>,
+);
