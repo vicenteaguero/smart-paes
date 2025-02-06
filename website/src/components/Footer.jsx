52bbf120744dc5f87f3b27f507d8dbc4ba9c5609
Vicente Agüero <vicenteaguero@uc.cl>
2025-02-05 22:39:48 -0300

    feat(website): :sparkles: Addde Footer Component for Project Attribution

---
+++ website/src/components/Footer.jsx
@@ -0,0 +1,14 @@
+// website/src/components/Footer.jsx
+
+const Footer = () => {
+  return (
+    <footer className="footer">
+      <p>SmartPAES es un proyecto Open Source.</p>
+      <p>
+        Creado por <a href="https://vicenteaguero.com">Vicente Agüero</a>.
+      </p>
+    </footer>
+  );
+};
+
+export default Footer;
