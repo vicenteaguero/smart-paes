fdd5663b4e8198cd1dcbac3095bff5ec4c88000a
Vicente Agüero <vicenteaguero@uc.cl>
2025-02-05 22:40:05 -0300

    feat(website): :sparkles: Added Home Page for React App

---
+++ website/src/pages/Home.jsx
@@ -0,0 +1,13 @@
+// website/src/pages/Home.jsx
+
+const Home = () => {
+  return (
+    <div className="home">
+      <h1>Bienvenido a SmartPAES</h1>
+      <h2>Prepara la PAES de forma inteligente.</h2>
+      <p>Página en construcción...</p>
+    </div>
+  );
+};
+
+export default Home;
