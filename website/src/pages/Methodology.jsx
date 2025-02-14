75e34f202caa630a1d2299339d8813e6288332ce
Vicente Agüero <vicenteaguero@uc.cl>
2025-02-14 20:38:22 -0300

    feat(website): :sparkles: Added a Methodology Page (fix: finish proof)

--- website/src/pages/Methodology.jsx
+++ website/src/pages/Methodology.jsx
@@ -1,10 +1,122 @@
 // website/src/pages/Methodology.jsx
 
+import React from 'react';
+
 const Methodology = () => {
   return (
     <div className="methodology">
-      <h2>Metodología</h2>
-      <p>Información sobre la metodología de SmartPAES.</p>
+      <h1>Metodología "No estudies como Nacional"</h1>
+      <p>¿Sabes qué porcentaje de estudiantes logran los 1000 pts en la PAES?</p>
+      <blockquote>Aproximadamente un 0.5% de todos los estudiantes.</blockquote>
+      <p>En otras palabras, apenas 1 de cada 200. ¿Qué sucede con los otros 199?</p>
+
+      <p>¿Sabías que en la PAES Admisión 2025...</p>
+      <ul>
+        <li>El 50% de los estudiantes logró menos de 600 puntos en la Prueba de Matemática M1.</li>
+        <li>El 50% de los estudiantes logró menos de 400 puntos en la Prueba de Matemática M2. Algo así, como 13 buenas de 50 preguntas.</li>
+      </ul>
+
+      <p>
+        ¿Y si te dijera que hay una forma de aumentar tu puntaje drásticamente
+        sin "matarse estudiando"?
+      </p>
+      <p>
+        Si quieres ser puntaje nacional, estudia. Yo lo fui en 2018, en la PSU de
+        matemáticas (ahora, PAES). Estudié muchísimo. Más de lo que jamás has
+        estudiado. Durante meses, solo estudié para la prueba.
+      </p>
+      <p>
+        ¿Realmente quieres ser nacional? Para ser nacional debes de conocer todos
+        los contenidos al derecho y al revés. ¿Pero... y tú? ¿Por qué deberías
+        estudiar del mismo modo que aquellos que buscan un puntaje perfecto?
+      </p>
+      <p>
+        ¿No existe un método de estudio paralelo? Lograr un buen puntaje para
+        entrar a la carrera que quiero... sin estudiar horas y horas todos los
+        contenidos.
+      </p>
+      <p>
+        Te presento la metodología: <strong>No estudies como Nacional</strong>.
+      </p>
+
+      <hr />
+
+      <p>
+        El principal objetivo de la PAES no es medir tu inteligencia... es
+        filtrar. No hay suficientes cupos en la educación superior para todos
+        los que salen de 4to medio. Si cuentas, además, todas las personas que
+        están dando la prueba por segunda y tercera vez, pues... hay mucha gente
+        compitiendo por unos pocos puestos.
+      </p>
+      <p>
+        Es decir, si quieres entrar a la carrera que quieres... no debes ser más
+        inteligente. La PAES no mide inteligencia. Tu objetivo es ganarle a la
+        prueba. Ganarle a la PAES. Y existen dos herramientas que necesitarás
+        para esto... el descarte y la suerte.
+      </p>
+
+      <hr />
+
+      <h2>La suerte juega un rol fundamental</h2>
+      <p>
+        El día de la PAES, será un día díficil. Tendrás naúseas, estarás
+        inseguro... ¿y si quedo en blanco? ¿y si me quedo sin tiempo? ¿y si...
+        ¡mil cosas! Todo puede pasar. No hay nadie que no esté en manos de la
+        suerte ese día. Incluso yo lo estuve cuando fui puntaje nacional.
+      </p>
+      <p>
+        Yo respondí una pregunta al azar. De verdad... no tenía ni idea. ¿Le
+        habré acertado? ¿Habrá sido pregunta piloto? ¿Mi cálculo estaba bien? ¿Me
+        equivoqué en el cálculo pero la respuesta me dio bien? No sé. Pero obtuve
+        los 850 puntos (en ese entonces). La suerte estuvo de mi lado.
+      </p>
+      <p>
+        Pero la suerte es solo un componente del éxito. Aunque sea un factor
+        pequeño, un 5% del resultado... lo puede cambiar todo. Si no quedaste por
+        un solo punto... ese 5% de suerte te habría dejado en tu carrera soñada.
+      </p>
+      <p>
+        ¿Y si pudiese colocar la suerte a mi favor? ¿Y si en vez de suerte...
+        hablamos de probabilidad? ¿Y si fuese posible aumentar tu suerte en la
+        PAES?
+      </p>
+
+      <hr />
+
+      <h2>El maestro del descarte</h2>
+      <p>
+        Alguna vez tuve problemas para tomar decisiones. Sobrepensaba muchísimo...
+        ¿y si ocurre esto? ¿y si ocurre esto otro? Me bloqueaba. No podía dejar
+        de pensar en todos los escenarios posibles.
+      </p>
+      <p>
+        Un mentor que tuve, me dijo que era normal. Que a él también le había
+        ocurrido. Pero que había encontrado la solución. Su estrategia era la
+        siguiente... pensaba rápidamente en todos esos escenarios... tomaba la
+        lista de decisiones y descartaba rápidamente todas las malas decisiones,
+        las que te llevarían a malos escenarios. Y luego, tomaba rápidamente una
+        de las decisiones restantes. Él sabía que tal vez no sería la mejor
+        decisión del mundo, pero sería dentro de las buenas. Pues, es mucho mejor
+        descartar las malas opciones, que perder oportunidades por querer elegir
+        el mejor escenario. La vida no es perfecta. Pero será buena, mientras
+        descartes lo malo.
+      </p>
+      <p>
+        De la misma forma, así funciona la PAES. Los estudiantes que quieren
+        sacar 1000 puntos, necesitan saber cuál es la respuesta correcta a cada
+        pregunta... igual que cuando quieres tomar la mejor decisión en tu vida.
+        Pero esto es muy díficil. Aunque te vuelvas muy bueno en esto, es
+        probable que te termines equivocando. El esfuerzo que requiere esta
+        estrategia es muy grande.
+      </p>
+      <p>
+        ¿Y qué puedo hacer entonces? No estudies para responder las preguntas
+        correctamente. Estudia para descartar las malas. Si combinas estrategias
+        para aumentar tu "suerte" (probabilidades de responder bien) junto con
+        descartar las respuestas incorrectas... verás que tu puntaje crecerá
+        muchísimo en muy poco tiempo.
+      </p>
+      <p>¿No me crees? Pues, te lo puedo demostrar.</p>
     </div>
   );
 };
